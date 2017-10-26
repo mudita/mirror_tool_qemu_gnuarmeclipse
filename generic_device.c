d1db4f9de5d77dcfa4679e3a5e44d72ecaf1e402
Konrad Traczyk <k.traczyk@mudita.com>
2017-10-26 12:43:17 +0200

    Added some callbacks

--- generic_device.c
+++ generic_device.c
@@ -6,17 +6,20 @@
  */
 
 #include "qom/object.h"
+#include "hw/cortexm/peripheral.h"
+#include "generic_device.h"
 
-#define TYPE_STM32_GENERIC_DEBUG_DEVICE     TYPE_STM32_PREFIX "gen-deb-dev" TYPE_PERIPHERAL_SUFFIX
+void generic_debug_device_instance_init_callback(Object *obj);
+void generic_debug_device_class_init_callback(ObjectClass *klass, void *data);
 
 static const TypeInfo generic_debug_device_type_info =
 {
     .name = TYPE_STM32_GENERIC_DEBUG_DEVICE,
-    .parent = TYPE_STM32_GPIO_PARENT,
+    .parent = TYPE_PERIPHERAL,
     .instance_init = generic_debug_device_instance_init_callback,
-    .instance_size = sizeof(STM32GPIOState),
+    .instance_size = sizeof(GenericDeviceState_t),
     .class_init = generic_debug_device_class_init_callback,
-    .class_size = sizeof(STM32GPIOClass)
+    .class_size = sizeof(STM32GenericDebugDeviceClass)
 /**/
 };
 
@@ -25,36 +28,71 @@ void register_debug_device_type()
     type_register_static(&generic_debug_device_type_info);
 }
 
-void generic_debug_device_instance_init_callback()
+void generic_debug_device_realize_callback(DeviceState *dev, Error **errp)
 {
+    // Call parent realize().
+    if (!cm_device_parent_realize(dev, errp, TYPE_STM32_GENERIC_DEBUG_DEVICE)) {
+        return;
+    }
+
+    STM32MCUState *mcu = stm32_mcu_get();
+    CortexMState *cm_state = CORTEXM_MCU_STATE(mcu);
+
+    GenericDeviceState_t *state = TYPE_STM32_GENERIC_DEBUG_DEVICE(dev);
+    // First thing first: get capabilities from MCU, needed everywhere.
+    state->capabilities = mcu->capabilities;
+
+    Object *obj = OBJECT(dev);
+
+    state->nvic = CORTEXM_NVIC_STATE(cm_state->nvic);
+
+    cm_object_property_set_int(obj, GENERIC_DEBUG_DEVICE_BUFFER_ADDRESS, "mmio-address");
+    cm_object_property_set_int(obj, GENERIC_DEBUG_DEVICE_BUFFER_SIZE, "mmio-size-bytes");
+
+    peripheral_create_memory_region(obj);
 
 }
 
-void generic_debug_device_class_init_callback()
+void generic_debug_device_instance_init_callback(Object *obj)
 {
+    GenericDeviceState_t *state = GENERIC_DEVICE_STATE(obj);
 
+    state->bareMetalSideBuffer = GENERIC_DEBUG_DEVICE_BUFFER_ADDRESS;
+    state->bareMetalSideBufferSize = GENERIC_DEBUG_DEVICE_BUFFER_SIZE;
 }
+
+void generic_debug_device_class_init_callback(ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+
+    dc->reset = generic_debug_device_reset_callback;
+    dc->realize = generic_debug_device_realize_callback;
+
+
+    PeripheralClass *per_class = PERIPHERAL_CLASS(klass);
+    per_class->is_enabled = generic_debug_device_is_enabled;
+}
+
 
 Object* generic_debug_device_create(Object *parent)
 {
     char child_name[10];
     snprintf(child_name, sizeof(child_name) - 1, "GenDebDev");
     // Passing a local string is ok.
-    Object *gpio = cm_object_new(parent, child_name, TYPE_STM32_GPIO);
-    int i;
+    Object *genDbgDev = cm_object_new(parent, child_name, TYPE_STM32_GENERIC_DEBUG_DEVICE);
+//    int i;
 
-    object_property_set_int(gpio, index, "port-index", NULL);
 
-    cm_object_realize(gpio);
+    cm_object_realize(genDbgDev);
 
-    for (i = 0; i < STM32_GPIO_PIN_COUNT; ++i) {
-        /* Connect GPIO outgoing to EXTI incoming. */
-        cm_irq_connect(DEVICE(gpio), STM32_IRQ_GPIO_EXTI_OUT, i,
-                cm_device_by_name(DEVICE_PATH_STM32_EXTI), STM32_IRQ_EXTI_IN,
-                i);
-    }
+//    for (i = 0; i < STM32_GPIO_PIN_COUNT; ++i) {
+//        /* Connect GPIO outgoing to EXTI incoming. */
+//        cm_irq_connect(DEVICE(gpio), STM32_IRQ_GPIO_EXTI_OUT, i,
+//                cm_device_by_name(DEVICE_PATH_STM32_EXTI), STM32_IRQ_EXTI_IN,
+//                i);
+//    }
 
-    return gpio;
+    return genDbgDev;
 }
 
 type_init(register_debug_device_type)
--- generic_device.h
+++ generic_device.h
@@ -8,6 +8,48 @@
 #ifndef PULL_GENERIC_DEVICE_H_
 #define PULL_GENERIC_DEVICE_H_
 
+#include <hw/cortexm/peripheral.h>
+#include <hw/cortexm/stm32/capabilities.h>
+
+#define GENERIC_DEBUG_DEVICE_BUFFER_ADDRESS         0x50000000
+#define GENERIC_DEBUG_DEVICE_BUFFER_SIZE            (64*1024)
 
 
+#define TYPE_STM32_GENERIC_DEBUG_DEVICE     TYPE_STM32_PREFIX "gen-deb-dev" TYPE_PERIPHERAL_SUFFIX
+
+typedef struct {
+    // private:
+    PeripheralClass parent_class;
+    // public:
+
+    // None, so far.
+} STM32GenericDebugDeviceClass;
+
+// Class definitions.
+#define STM32_GENERIC_DEVICE_GET_CLASS(obj) \
+    OBJECT_GET_CLASS(STM32GenericDebugDeviceClass, (obj), TYPE_STM32_GENERIC_DEBUG_DEVICE)
+#define STM32_GENERIC_DEVICE_CLASS(klass) \
+    OBJECT_CLASS_CHECK(STM32GenericDebugDeviceClass, (klass), TYPE_STM32_GENERIC_DEBUG_DEVICE)
+
+// ----------------------------------------------------------------------------
+
+
+// Instance definitions.
+#define GENERIC_DEVICE_STATE(obj) \
+    OBJECT_CHECK(GenericDeviceState_t, (obj), TYPE_STM32_GENERIC_DEBUG_DEVICE)
+
+typedef struct
+{
+    // private:
+    PeripheralState parent_obj;
+    // public:
+
+    const STM32Capabilities *capabilities;
+
+    CortexMNVICState *nvic;
+
+    void*       bareMetalSideBuffer;
+    uint32_t    bareMetalSideBufferSize;
+
+}GenericDeviceState_t;
 #endif /* PULL_GENERIC_DEVICE_H_ */
